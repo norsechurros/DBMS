c439f21a2fc77fe0de73c452d2fa9e4f9f3cd6ab
pranavc33 <pranavchavare33@gmail.com>
2024-01-08 09:28:30 +0530

    first

---
+++ bankApp.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class bankApp
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of customers (n): ");
+        int n = sc.nextInt();
+
+        bankApp ob[] = new bankApp();
+
+    }
+}
